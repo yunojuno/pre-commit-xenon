c7e19e94a58937da5d0fc99cb00917d2e0688d99
Hugo Rodger-Brown <hugo@yunojuno.com>
2019-11-18 15:19:56 +0000

    Bump version to v0.1

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup
 
+__version__ = "0.1"
 
 setup(
     name='pre_commit_dummy_package',
-    version='0.0.0',
+    version=__version__,
     install_requires=['xenon'],
 )
